ce856d48e8e3e7aa532ca1f059c2174021db12c9
iradakhf <iradakhf@code.edu.az>
2022-07-09 21:42:22 +0400

    add all

---
+++ Invoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Invoice
+    {
+       public int account;
+       public string customer;
+       public string provider;
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public Invoice()
+        {
+
+        }
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+        public double CostCalculation ( bool needVat, double productValue, int Quantity)
+        {  
+            double vat = productValue* 0.18;
+            if (needVat == true)
+            {
+                productValue = productValue * Quantity + vat;
+            }
+            else
+            {
+                productValue *= Quantity;
+            }
+            return productValue;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Invoice();
+            obj.Article="LapTop";
+            //obj.Article = "SD Card";
+            //obj.Article = "USB";
+           
+           
+            Invoice invoice = new Invoice(1254, "Ird", "SuperMarket");
+            
+           
+            double cost = 0;
+            if (obj.Article == "LapTop")
+            {   cost = obj.CostCalculation(true, 1200, 4);
+                Console.WriteLine($"{ invoice.account}: { invoice.customer}: { invoice.provider}");
+                Console.WriteLine($"{obj.Article} : {cost} azn");
+            }    
+            else if (obj.Article== "SD Card")
+            {
+                cost = obj.CostCalculation(true, 30, 2);
+                Console.WriteLine($"{cost} {invoice}");
+            }
+            else
+            {
+                cost = obj.CostCalculation(false, 12, 1);
+                Console.WriteLine($"{cost} {invoice}");
+            }
+
+        }
+    }
+}
